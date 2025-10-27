9bfdaad3ddcd4b69a147c19fa163cd7cf4a309ce
Adieltheanalyst <adielngugi@gmail.com>
2025-10-27 21:13:42 +0300

    new commit

---
+++ ReAct.py
@@ -0,0 +1,66 @@
+from typing import TypedDict,Annotated,Sequence
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_groq import ChatGroq
+from langchain_core.messages import BaseMessage,ToolMessage,SystemMessage
+from langgraph.graph import StateGraph, END, START
+from dotenv import load_dotenv
+from langgraph.graph.messages import add_messages
+from langgraph.prebuilt import ToolNode
+from langchain_core.tools import tool
+import os 
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+llm=ChatGroq(model="qwen/qwen3-32b",api_key=os.getenv("GROQ_API_KEY"))
+
+@tool
+def add(a: int, b:int):
+    """This is an addition function that adds 2 numbers together"""
+    return a+b
+tools= [add]
+model=ChatGroq(model="qwen/qwen3-32b",api_key=os.getenv("GROQ_API_KEY")).bind_tools(tools)
+
+def model_call(state:AgentState)->AgentState:
+    system_prompt= SystemMessage(content=
+            "You are my AI assistant, please anser my query to the best of your ability.")
+    response=model.invoke([system_prompt]+state["messages"])
+    return {"messages":[response]}
+
+def should_continue(state:AgentState):
+    messages=state["messages"]
+    last_message=messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent",model_call)
+
+tool_node=ToolNode(tools=tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END
+    },
+)
+graph.add_edge("tools","our_agent")
+
+app=graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message=s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.preety_print()
