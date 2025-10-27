05fbe36a0f0afdb89a2129ecba992f6c523b12c1
Adieltheanalyst <adielngugi@gmail.com>
2025-10-27 16:19:45 +0300

    working perfectly but now I need to add memory

--- Agent_Bot.py
+++ Agent_Bot.py
@@ -1,3 +1,35 @@
 from typing import TypedDict,List
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph, END, START
+from dotenv import load_dotenv
+import os 
+
+
+
+load_dotenv()
+
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+llm=ChatGroq(model="qwen/qwen3-32b",api_key=os.getenv("GROQ_API_KEY"))
+def process(state: AgentState)-> AgentState:
+    response=llm.invoke(state["messages"])
+    print(f"\nAI: {response.content}")
+    return state
+
+graph=StateGraph(AgentState)
+graph.add_node("process",process)
+graph.add_edge(START,"process")
+graph.add_edge("process", END)
+
+agent=graph.compile()
+
+user_input=input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input= input("Enter: ")
+
+
+# agent.invoke({"messages": [HumanMessage(content=user_input)]})
